2908a5e0ccfb92f3a43e82b66be52375063781f9
architquickproc <160396134+architquickproc@users.noreply.github.com>
2025-01-19 17:25:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-3xl font-bold underline">Namaste react</header>
+    </div>
+  );
+}
+
+export default App;
